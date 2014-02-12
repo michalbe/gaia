a0b6e1bba70d27154c8ca6bf7e89bb0469b5c972
Michal Budzynski <michal@virtualdesign.pl>
2014-02-12 15:16:42 +0100

    [NFC Contacts] Lint fixes

--- apps/communications/contacts/js/nfc.js
+++ apps/communications/contacts/js/nfc.js
@@ -1,21 +1,21 @@
 var NFC = function() {
   console.log('NFC ready');
   var mozNfc = window.navigator.mozNfc;
-  
+
   var startListening = function() {
     console.log('start listening!');
     mozNfc.onpeerready = function(e) {
       console.log('-----PEER READY!', e);
     };
-  }
-  
+  };
+
   var stopListening = function() {
     console.log('stop listening!');
     mozNfc.onpeerready = null;
-  }
-    
+  };
+
   return {
     startListening: startListening,
     stopListening: stopListening
-  }
+  };
 }();
