26ef70a48d3b656d37d955e605b7c8b492c112a5
Michal Budzynski <michal@virtualdesign.pl>
2014-02-17 17:32:08 +0100

    [Contacts NFC] Lint fixes

--- apps/communications/contacts/test/unit/mock_mozNfc.js
+++ apps/communications/contacts/test/unit/mock_mozNfc.js
@@ -1,10 +1,12 @@
 'use strict';
 
+/* exported MockMozNfc */
+
 var MockMozNfc = {
   onpeerready: null,
   getNFCPeer: function(event) {
     return {
       sendNDEF: function(records){}
-    }
+    };
   }
 };
