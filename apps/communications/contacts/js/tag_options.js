4a186346e46a7b2c7b8c77a447cb9d57eda299c1
Michal Budzynski <michal@virtualdesign.pl>
2014-06-02 14:17:42 +0800

    test

--- apps/communications/contacts/js/tag_options.js
+++ apps/communications/contacts/js/tag_options.js
@@ -1,45 +1,33 @@
 'use strict';
-/* global _ */
 /* exported TAG_OPTIONS */
 
 console.log('TAG OPTIONS FILE LOADED');
+console.log('ready?', navigator.mozL10n.isReady);
 
-var GET_TAG_OPTIONS = function() {
-  return {
-    get 'phone-type'() {
-      return [
-        {type: 'mobile', value: _('mobile')},
-        {type: 'home', value: _('home')},
-        {type: 'work', value: _('work')},
-        {type: 'personal', value: _('personal')},
-        {type: 'faxHome', value: _('faxHome')},
-        {type: 'faxOffice', value: _('faxOffice')},
-        {type: 'faxOther', value: _('faxOther')},
-        {type: 'other', value: _('other') }
-      ];
-    },
-    get 'email-type'() {
-      return [
-        {type: 'personal', value: _('personal')},
-        {type: 'home', value: _('home')},
-        {type: 'work', value: _('work')},
-        {type: 'other', value: _('other')}
-      ];
-    },
-    get 'address-type'() {
-      return [
-        {type: 'current', value: _('current')},
-        {type: 'home', value: _('home')},
-        {type: 'work', value: _('work')}
-      ];
-    },
-    get 'date-type'() {
-      return [
-        {type: 'birthday', value: _('birthday')},
-        {type: 'anniversary', value: _('anniversary')}
-      ];
-    }
-  };
+var TAG_OPTIONS = {
+  'phone-type' : [
+    {type: 'mobile', value: _('mobile')},
+    {type: 'home', value: _('home')},
+    {type: 'work', value: _('work')},
+    {type: 'personal', value: _('personal')},
+    {type: 'faxHome', value: _('faxHome')},
+    {type: 'faxOffice', value: _('faxOffice')},
+    {type: 'faxOther', value: _('faxOther')},
+    {type: 'other', value: _('other')}
+  ],
+  'email-type' : [
+    {type: 'personal', value: _('personal')},
+    {type: 'home', value: _('home')},
+    {type: 'work', value: _('work')},
+    {type: 'other', value: _('other')}
+  ],
+  'address-type' : [
+    {type: 'current', value: _('current')},
+    {type: 'home', value: _('home')},
+    {type: 'work', value: _('work')}
+  ],
+  'date-type': [
+    {type: 'birthday', value: _('birthday')},
+    {type: 'anniversary', value: _('anniversary')}
+  ]
 };
-
-var TAG_OPTIONS = GET_TAG_OPTIONS();
