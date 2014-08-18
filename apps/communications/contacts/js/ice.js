722e06c57643a09598e8f55c9cc4b0ffa4c7f565
Michal Budzynski <michal@virtualdesign.pl>
2014-08-18 14:56:25 -0500

    [GAIA] - reorganization of the DOM handlers

--- apps/communications/contacts/js/ice.js
+++ apps/communications/contacts/js/ice.js
@@ -5,13 +5,11 @@ var contacts = window.contacts || {};
 contacts.ICE = (function() {
   var iceSettingsPanel,
     iceSettingsBack,
-    iceContactItem1,
-    iceContactItem2,
-    iceContactCheckbox1,
-    iceContactCheckbox2,
-    iceContactButton1,
-    iceContactButton2,
-    iceScreenLoaded = false;
+    iceContactItems = [],
+    iceContactCheckboxes = [],
+    iceContactButtons = [],
+    iceScreenLoaded = false,
+    ICE_CONTACTS_KEY = 'ice-contacts';
 
   var init = function ice_init() {
     if (iceScreenLoaded) {
@@ -21,14 +19,17 @@ contacts.ICE = (function() {
     iceSettingsPanel = document.getElementById('ice-settings');
     iceSettingsBack = document.getElementById('ice-settings-back');
 
-    iceContactItem1 = document.getElementById('ice-contacts-1-switch');
-    iceContactItem2 = document.getElementById('ice-contacts-2-switch');
-    iceContactCheckbox1 = iceContactItem1
-                          .querySelector('[name="ice-contact-1-enabled"]');
-    iceContactCheckbox2 = iceContactItem2
-                          .querySelector('[name="ice-contact-2-enabled"]');
-    iceContactButton1 = document.getElementById('select-ice-contact-1');
-    iceContactButton2 = document.getElementById('select-ice-contact-2');
+    iceContactItems.push(document.getElementById('ice-contacts-1-switch'));
+    iceContactItems.push(document.getElementById('ice-contacts-2-switch'));
+
+    iceContactCheckboxes.push(iceContactItems[0]
+                          .querySelector('[name="ice-contact-1-enabled"]'));
+    iceContactCheckboxes.push(iceContactItems[1]
+                          .querySelector('[name="ice-contact-2-enabled"]'));
+    iceContactButtons.push(document.getElementById('select-ice-contact-1'));
+    iceContactButtons.push(document.getElementById('select-ice-contact-2'));
+
+    getICEContactsFromInternalStore(setInitialButtonsState);
 
     // ICE Events handlers
     iceSettingsBack.addEventListener('click', function(){
@@ -37,22 +38,55 @@ contacts.ICE = (function() {
 
     // Temporary logic & listeners, this will be removed or improved
     // in Bug 1042584 in 2.1S1 or S2
-    iceContactItem1.addEventListener('click', function(){
-      var status = iceContactCheckbox1.checked;
-      iceContactCheckbox1.checked = !status;
-      iceContactButton1.disabled = status;
+    iceContactItems.forEach(function(item, index) {
+      item.addEventListener('click', function(i) {
+        return function(evt) {
+          var status = iceContactCheckboxes[i].checked;
+          iceContactCheckboxes[i].checked = !status;
+          iceContactButtons[i].disabled = status;
+        }
+      }(index));
     });
-    iceContactItem2.addEventListener('click', function(){
-      var status = iceContactCheckbox2.checked;
-      iceContactCheckbox2.checked = !status;
-      iceContactButton2.disabled = status;
-    });
-    [iceContactButton1, iceContactButton2].forEach(function(element){
-      element.addEventListener('click', function(){
+
+    iceContactButtons.forEach(function(element){
+      element.addEventListener('click', function() {
           showSelectList();
       });
     });
+
     iceScreenLoaded = true;
+  };
+
+  var getICEContactsFromInternalStore = function(callback) {
+    var iceContactsIds = [
+      {
+        id: undefined,
+        active: false
+      },
+      {
+        id: undefined,
+        active: false
+      }
+    ];
+
+    window.asyncStorage.getItem(ICE_CONTACTS_KEY, function(data) {
+      if (data) {
+        if (data[0]) {
+          iceContactsIds[0] = data[0];
+        }
+        if (data[1]) {
+          iceContactsIds[1] = data[1];
+        }
+      }
+      callback(iceContactsIds);
+    });
+  };
+
+  var setInitialButtonsState = function(iceContactsIds) {
+    var setButtonState = function(id) {
+
+    };
+    //for iceContactsIds
   };
 
   var showSelectList = function showSelectList() {
